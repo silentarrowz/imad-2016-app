ee831b5732f9e8f1279b0147ccfa351f531a923f
faraz <silentarrowz@gmail.com>
2016-09-18 19:59:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,6 +100,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function(req,res){
     //articleName == articleOne
     //articles[articleName] =={} content object for article one
+    var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
