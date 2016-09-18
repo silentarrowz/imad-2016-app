27b340fd5981687ddebf5696dae2f9de98c85969
faraz <silentarrowz@gmail.com>
2016-09-18 17:35:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,11 +58,11 @@ app.get('/article-one', function(req,res){
   res.send(createTemplate(articleOne));
 });
 
-app.get('/article-two.html', function(req,res){
+app.get('/article-two', function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
-app.get('/article-three.html', function(req,res){
+app.get('/article-three', function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
