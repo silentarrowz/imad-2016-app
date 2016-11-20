8d6eed69c77622f3557d638a2bdb5cb85e5c99ef
faraz <silentarrowz@gmail.com>
2016-11-20 15:16:31 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,68 +1,36 @@
 window.onload = function(){
-  var counter=0;
-   
-    var button = document.getElementById('submit_btn');
-    button.onclick = function(){
-    	 //counter=counter+1; this was just client side without visiting server
-    	 
-    	 //this is requesting server for information
-    	 //Making a request to counter endpoint
-    	 //Create request object
-    	 var request = new XMLHttpRequest();
-         var nameInput = document.getElementById('name');
-         var name=nameInput.value;
-         console.log('name is: ',name);
-    	 //Capture the response and store it in a variable
-    	 request.onreadystatechange = function(){
-    	 	if(request.readyState === XMLHttpRequest.DONE){
-    	 		//take some action
-    	 		if(request.status ===200){
-    	 			var names=request.responseText;//this gets a string
-                    console.log('names1 is: ',names);
-                    names = JSON.parse(names); //converting string to array
-                    console.log('names is: ',names);
-                    var list='';
-                    for(var i=0;i<names.length;i++){
-                    list=list +'<li>'+names[i]+"</li>";
-                    console.log('list is: ',list);
-                    }
-            var ul = document.getElementById('namelist');
-            ul.innerHTML = list;
-    	 		}
-    	 	}
-    	 
-    	 	
-    	 };//button onclick function ends
+  
 
-    	 //Make the request
-    	 request.open('GET','http://silentarrowz.imad.hasura-app.io/submit-name?name='+name,true);
-    	 request.send(null);
-    	
-    }; //button onclick function ends
-
-  /*
-    var nameInput = document.getElementById('name');
-    
-    var submit = document.getElementById('submit_btn');
-    submit.onclick = function(){
-        var names=['name1','name2','name3','name4'];
-              var list='';
-    for(var i=0;i<names.length;i++){
-        list=list +'<li>'+names[i]+"</li>";
-
+var twitterButton = document.getElementById('tweetButton');
+twitterButton.onclick = function(){
+    var searchFor =document.getElementById('tweetSearch');
+    var searchForTerm = searchFor.value;
+    console.log('search for keyword : ', searchForTerm);
+var twitterxr = new XMLHttpRequest();
+twitterxr.onreadystatechange = function(){
+if(twitterxr.readyState ===XMLHttpRequest.DONE && twitterxr.status ===200){
+    var newTweets = JSON.parse(twitterxr.responseText);
+    console.log("newTweets is : ",newTweets);
+    var tweetz='';
+    var t;
+    for(var i=0;i<newTweets.length;i++){
+            t=i+1;
+         tweetz=tweetz+ "<p>"+ "<span class='twitlabel'>"+ "TWEET "+t+" :"+"</span>"+ newTweets[i] + "</p>"+ "<hr>";
     }
-        var ul = document.getElementById('namelist');
-        ul.innerHTML = list;
-        
-    }
-*/ 
+    console.log('tweetz is : ',tweetz);
+    var tweetHere = document.getElementById('tweetz');
+    tweetHere.innerHTML = tweetz;
+}
 
 
-
+};//twitterxr onreadystatechange function ends
 
  //Make the request
-         //request.open('GET','http://localhost:8080/submit_comment?comment='+comment,true);
-         //request.send(null);
+         twitterxr.open('GET','http://silentarrowz.imad.hasura-app.io/t/'+searchForTerm,true);
+         twitterxr.send(null);
+};//twitterbutton onclick function ends
+
+
 var submitButton = document.getElementById('submit_btn');
 submitButton.onclick = function(){
     var request = new XMLHttpRequest();
@@ -81,9 +49,9 @@ submitButton.onclick = function(){
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username:username,password:password}));
 };
-  
-  
-  
+
+
+
 };//window onload function ends
 
 
