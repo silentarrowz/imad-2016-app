453ebabc5056c241416b9afed0e504c2ce4b14f5
faraz <silentarrowz@gmail.com>
2016-11-22 16:29:57 +0530

    [imad-console] Updates news.js

--- news.js
+++ news.js
@@ -1,34 +1,95 @@
 window.onload = function(){
 var displayNews = document.getElementById('currentNews');
-var newsButton = document.getElementById('getnews');
+var sportsButton = document.getElementById('sports');
+var politicsButton = document.getElementById('current');
+var financeButton = document.getElementById('finance');
+
+sportsButton.onclick = buttonClick('sports'); //this gets called onload
+
+
+//this will get called on click
+sportsButton.onclick = function(){
+	buttonClick('sports');
+}
+//this will get called on click
+politicsButton.onclick = function(){
+buttonClick('politics');
+}
+
+//this will get called on click
+financeButton.onclick = function(){
+ buttonClick('finance');
+}
+/*
 newsButton.onclick = function(){
-	alert('the button is clicked');
 	var newsxr = new XMLHttpRequest();
 	newsxr.onreadystatechange = function(){
 		if(newsxr.readyState ===XMLHttpRequest.DONE && newsxr.status ===200){
 			var currentNews = JSON.parse(newsxr.responseText);
-			var currentArticles = currentNews['articles'];
-			var numberArticles = currentNews['articles'].length;
-			var newsDisplay ='';
-			var author;
-			var title;
-			var description;
-			var urlToImage;
-			for(var i=0;i<numberArticles;i++){
-				author = currentArticles[i]['author'];
-				title = currentArticles[i]['title'];
-				description = currentArticles[i]['description'];
-				urlToImage = currentArticles[i]['urlToImage'];
-				 newsDisplay = newsDisplay + "<p>"+"<span class='title'>"+  title+"</span>"+  "<br>"+description+"<br>"+"<img src='"+urlToImage+"'</img>"+"</p>";
-			}
-			alert('displaying the news now');
+			var doc = currentNews.response.docs;
+			var docslength = doc.length;
+			var headline;
+			var mainLead;
+			var imageUrl='';
+			console.log('docslength is : ',docslength);
+			
+			console.log('news is : '+doc[0].headline.main+doc[0]['lead_paragraph']);
 			console.log('current news is : ',currentNews);
+			var newsDisplay ='';
+			for(var i=0;i<docslength;i++){
+				headline = doc[i].headline.main;
+				mainLead = doc[i]['lead_paragraph'];
+				if(doc[i].multimedia.length===3){
+					imageUrl ='http://www.nytimes.com/'+ doc[i].multimedia[0].url;
+					newsDisplay = newsDisplay + "<p>" +"<img src='"+imageUrl+"'</img>"+"<br>"+ "<span class='title'>"+  headline+"</span>"+  "<br>"+mainLead+"<br>" +"</p>";
+				}else{
+			newsDisplay = newsDisplay + "<p>" + "<span class='title'>" +  headline+"</span>"+  "<br>"+mainLead +"</p>";	
+				 }
+			}//for loop ends
+
 			displayNews.innerHTML = newsDisplay;
-		}
+			
+		}//if block ends
 	};//on state change
 
-	newsxr.open('GET','https://newsapi.org/v1/articles?source=national-geographic&sortBy=top&apiKey=1af110441a8e4f72925f78344e58c2a4',true);
+	newsxr.open('GET','http://api.nytimes.com/svc/search/v2/articlesearch.json?q=sports&page=2&sort=newest&api-key=1a246f14cd594927b5290af9a9e2a3ca',true);
+	newsxr.send(null);
+};//button onclick function ends
+*/
+
+function buttonClick(topic){
+	var newsxr = new XMLHttpRequest();
+	newsxr.onreadystatechange = function(){
+		if(newsxr.readyState ===XMLHttpRequest.DONE && newsxr.status ===200){
+			var currentNews = JSON.parse(newsxr.responseText);
+			var doc = currentNews.response.docs;
+			var docslength = doc.length;
+			var headline;
+			var mainLead;
+			var imageUrl='';
+			console.log('docslength is : ',docslength);
+			
+			console.log('news is : '+doc[0].headline.main+doc[0]['lead_paragraph']);
+			console.log('current news is : ',currentNews);
+			var newsDisplay ='';
+			for(var i=0;i<docslength;i++){
+				headline = doc[i].headline.main;
+				mainLead = doc[i]['lead_paragraph'];
+				if(doc[i].multimedia.length===3){
+					imageUrl ='http://www.nytimes.com/'+ doc[i].multimedia[0].url;
+					newsDisplay = newsDisplay + "<p>" +"<img src='"+imageUrl+"'</img>"+"<br>"+ "<span class='title'>"+  headline+"</span>"+  "<br>"+mainLead+"<br>" +"</p>";
+				}else{
+			newsDisplay = newsDisplay + "<p>" + "<span class='title'>" +  headline+"</span>"+  "<br>"+mainLead +"</p>";	
+				 }
+			}//for loop ends
+
+			displayNews.innerHTML = newsDisplay;
+			
+		}//if block ends
+	};//on state change
+
+	newsxr.open('GET','http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+topic+'&page=2&sort=newest&api-key=1a246f14cd594927b5290af9a9e2a3ca',true);
 	newsxr.send(null);
 };//button onclick function ends
 
-};// window onclick function ends
+};//window onload function ends
