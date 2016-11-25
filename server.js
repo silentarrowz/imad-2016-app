34263b32ef80e4bd3f87d2be0ff6878b8be29c04
faraz <silentarrowz@gmail.com>
2016-11-25 22:13:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -166,7 +166,7 @@ app.post('/login',function(req,res){
           var salt = dbString.split('$')[2];
           var hashedPassword = hash(password,salt);//creating a hash based on password submitted and the original salt
           if(hashedPassword===dbString){
-              res.send('user successfully logged in');
+              res.send('credentials are correct');
           }else{
               res.send('403').send('username/password is invalid');
           }
