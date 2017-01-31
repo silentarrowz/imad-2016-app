a8d9c29c15044d1aaeac32688fbd2b0aab44b9a5
faraz <silentarrowz@gmail.com>
2017-02-01 01:10:38 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	consumer_key:'56UIBVoiBmGSNA4PTjJ0jJxHs'
-	,consumer_secret:'PDppcL5hgCqXhM3gearBdrEKfRoLS5c01WTTNjkExK6ca3SB0Z'
-	,access_token:'12385392-GTpaJld8SG0gRxc1MmzArz6eRyGN0YANVzzTvv8hQ'
-	,access_token_secret:'SKT4kJv9xAxXog0VIrHuowUgDFqCenMqW2oKquaXwGTAT'
-}
